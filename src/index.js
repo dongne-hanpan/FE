702a427ae12a16c0ee99bf711b3fc6cc7a9ab777
IngJun <107452558+IngJun@users.noreply.github.com>
2022-08-30 16:24:02 +0900

    feat: theme provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import theme from "./shared/redux_j/themeStyle";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
 );
