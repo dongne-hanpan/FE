b6233fd055dc83a327b4a328edc23eb77c9ac775
youngdong82 <nfs82young@gmail.com>
2022-08-30 17:47:33 +0900

    feat: #1 Create HeaderAlerm.js
    Header 안에서 반복적으로 쓰이는 알람 컴포넌트를 분리 생성

---
+++ src/components/univ/HeaderAlerm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import green from '../../asset/green.png'
+import red from '../../asset/red.png'
+
+const HeaderAlerm = ({checked}) => {
+  //언제 녹색이고 언제 빨간색?
+  return(
+    <AlermComp>
+      <div className="alermImg">
+        {checked ? 
+        <img src={red} alt='checkToggle'/>
+        :
+        <></>
+        }
+      </div>
+      <div className="alermMsg">
+        최영준 님이 친구 신청을 보냈습니다.
+        </div>
+    </AlermComp>
+  )
+};
+
+export default HeaderAlerm;
+
+
+const AlermComp = styled.div`
+  width: 90%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  .alermImg{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 20px;
+    & img{
+      width: 10px;
+    }
+  }
+  .alermMsg{
+    font-size: var(--font-14);
+    padding-left: 100px;
+    color: var(--color-background);
+  }
+
+`
+
+
