44294fd66fad1f189af8cf53252043b459a71765
IngJun <107452558+IngJun@users.noreply.github.com>
2022-08-30 16:22:47 +0900

    feat: #4 create chatpage

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,12 +1,57 @@
-import React from 'react';
-
+import { useParams } from "react-router";
+import styled from "styled-components";
+import ChannelHeader from "../components/chatPage/channelHeader";
+import ChannelList from "../components/chatPage/channelList";
+import ChattingList from "../components/chatPage/chattingList";
+import UserList from "../components/chatPage/userList";
 
 const ChatPage = () => {
+  const params = useParams().channel_id;
   return (
-    <div>
-      ChatPage
-    </div>
-  )
-}
+    <MainPageWrapper>
+      {/* <ChannelHeader></ChannelHeader> */}
+      <BodyWrapper>
+        <MenuWrapper>
+          <ChannelList />
+          {params ? <UserList /> : null}
+        </MenuWrapper>
+        {params ? (
+          <ChattingWrapper>
+            <ChattingList></ChattingList>
+          </ChattingWrapper>
+        ) : null}
+      </BodyWrapper>
+    </MainPageWrapper>
+  );
+};
 
 export default ChatPage;
+
+const MainPageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BodyWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+`;
+const MenuWrapper = styled.div`
+  width: 350px;
+  height: 100%;
+  border-right: 0.1px solid grey;
+  display: flex;
+  background-color: ${(props) => props.theme.palette.blue};
+`;
+const ChattingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  /* background-color: white; */
+`;
