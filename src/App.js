cdce49262a047d9f3c7a96c9c81dac5884088e1f
youngdong82 <nfs82young@gmail.com>
2022-08-31 18:47:37 +0900

    FEAT: #7 Create and Connect SportsPage.js
    SportsPage.js 생성 및 App 컴포넌트의 라우터에 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/univ/Header';
-import ChatPage from './pages/ChatPage';
 import IndexPage from './pages/IndexPage'
 import Register from './pages/Register';
+import SportsPage from './pages/SportsPage';
+import ChatPage from './pages/ChatPage';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<IndexPage />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/:sports' element={<SportsPage />} />
           <Route path='/chat' element={<ChatPage />} />
         </Routes>
       </Router>
