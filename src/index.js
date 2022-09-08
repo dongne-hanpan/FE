7df5cabadfde3f018652a9b7fe8d1a34a67da882
youngdong82 <nfs82young@gmail.com>
2022-09-08 17:18:32 +0900

    DESIGN: #18 Create GlobalStyle in styled-components
    css 통일을 위해 styled-components	의 createGlobalStyle 메소드 사용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './shared/redux_d/configure';
 import './index.css';
 
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    .App {
+        position: relative;
+    }
+    body {
+        margin: 0px;
+        background-color: var(--color-background);
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
+    button {
+        outline: none;
+        border: none;
+        cursor: pointer;
+        padding: 0px;
+    }
+    input {
+        outline: none;
+        border: none;
+        cursor: pointer;
+    }
+    textarea {
+        outline: none;
+        border: none;
+        cursor: pointer;
+    }
+`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
+        <GlobalStyle />
         <App />
     </Provider>
 );
