fface812a8c9fde573fa35e3f341de3511188693
youngdong82 <nfs82young@gmail.com>
2022-09-20 01:15:51 +0900

    FEAT: #41 Create ChangeProfile modal
    ChangeProfile 모달 생성

---
+++ src/components/modal/ChangeProfile.js
@@ -0,0 +1,127 @@
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { clearModal } from '../../shared/redux/modules/modalSlice';
+import { updateProfileThunk } from '../../shared/redux/modules/userSlice';
+import ReuseBtn from '../reusable/ReuseBtn';
+
+
+const ChangeProfile = () => {
+  const dispatch = useDispatch();
+  const inputFile = useRef();
+  const [preview, setPreview] = useState(null);
+  const [imgData, setImgData] = useState(null);
+
+  const dropImg = (e) =>{
+    e.preventDefault();
+    const file = e.dataTransfer.files;
+    setImgData(file[0]);
+    const reader = new FileReader();
+    reader.readAsDataURL(file[0]);
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      if(base64){
+        const base64Sub = base64.toString();
+        setPreview(base64Sub);
+      }
+    }
+  };
+  const dragOverImg = (e) =>{
+    e.preventDefault();
+  }
+  const clickToLoad = () => {
+    inputFile.current.click();
+  }
+  const inputImgChange = (e) => {
+    const files = e.target.files;
+    setImgData(files[0]);
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onloadend = () => {
+      const base64 = reader.result;
+      if(base64){
+        const base64Sub = base64.toString();
+        setPreview(base64Sub);
+      }
+    }
+  };
+
+  const uploadProfile = () => {
+    if(imgData !== null){
+      let formData = new FormData();
+      formData.append("image", imgData);
+      dispatch(updateProfileThunk(formData));
+      dispatch(clearModal());
+    }
+  };
+
+  return(
+    <ChangeProfileComp>
+      <InputTitleBox>
+        <InputTitle>프로필 사진 변경</InputTitle>
+      </InputTitleBox>
+        {preview ? 
+          <PreviewImgBox onDrop={dropImg} onDragOver={dragOverImg}>
+            <PreviewImg src={preview} alt="preview" />
+          </PreviewImgBox>
+        :
+          <DropImgBox onDrop={dropImg} onDragOver={dragOverImg}>사진과 동영상을 여기에 끌어다 놓으세요</DropImgBox>
+        }
+          <Btns>
+            <ReuseBtn direc={'horiz'} styleType={'normal'} content={'컴퓨터에서 선택'} clickEvent={clickToLoad} />
+            <ReuseBtn direc={'horiz'} styleType={'normal'} content={'변경하기'} clickEvent={uploadProfile} />
+          </Btns>
+          <InvisibleInput type="file" ref={inputFile} onChange={inputImgChange} />
+
+    </ChangeProfileComp>
+  )
+};
+
+export default ChangeProfile;
+
+const ChangeProfileComp = styled.section`
+  width: 360px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+`
+const InputTitleBox = styled.div`
+  width: 360px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 12px;
+`
+const InputTitle = styled.div`
+  font-size: ${({theme}) => theme.fontSize.font_18};
+  font-weight: ${({theme}) => theme.fontWeight.medium};
+`
+const PreviewImgBox = styled.div`
+  width: 360px;
+  height: 360px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  overflow-y: hidden;
+`
+const PreviewImg = styled.img`
+  width: 100%;
+  object-fit: cover;
+`
+const DropImgBox = styled.div`
+  width: 360px;
+  height: 360px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 3px dashed ${({theme}) => theme.colors.gray};
+  margin-bottom: 30px;
+`
+const Btns = styled.div`
+  display: flex;
+`
+const InvisibleInput = styled.input`
+  visibility: hidden;
+`
